ad6d637fbea36478f98621cf9230cbbd7e7ac275
Wootever <Wootever@users.noreply.github.com>
2017-05-22 15:41:31 +0200

    panel: add smart dimming brightness control
    use "echo 0 > /sys/class/lcd/panel/smart_on" to disable smart dimming
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/video/fbdev/exynos/decon_8890/panels/dsim_backlight_dynamic.c
+++ drivers/video/fbdev/exynos/decon_8890/panels/dsim_backlight_dynamic.c
@@ -142,6 +142,14 @@ static unsigned char *get_gamma_from_index(struct dsim_device *dsim, int index)
 {
 	struct panel_private *panel = &dsim->priv;
 	struct SmtDimInfo *dimming_info = (struct SmtDimInfo *)panel->dim_info;
+#ifdef CONFIG_PANEL_SMART_DIMMING
+	struct SmtDimInfo *smart_dimming_info = (struct SmtDimInfo *)panel->smart_dim_info;
+
+	if (smart_dimming_info == NULL) {
+		dsim_err("%s: dimming info is NULL\n", __func__);
+		goto get_gamma_err;
+	}
+#endif
 
 	if (dimming_info == NULL) {
 		dsim_err("%s : dimming info is NULL\n", __func__);
@@ -151,6 +159,11 @@ static unsigned char *get_gamma_from_index(struct dsim_device *dsim, int index)
 	if (index > MAX_BR_INFO)
 		index = MAX_BR_INFO;
 
+#ifdef CONFIG_PANEL_SMART_DIMMING
+	if (panel->smart_on)
+		return (unsigned char *)smart_dimming_info[index].gamma;
+	else
+#endif
 	return (unsigned char *)dimming_info[index].gamma;
 
 get_gamma_err:
@@ -162,6 +175,14 @@ static unsigned char *get_elvss_from_index(struct dsim_device *dsim, int index, 
 {
 	struct panel_private *panel = &dsim->priv;
 	struct SmtDimInfo *dimming_info = (struct SmtDimInfo *)panel->dim_info;
+#ifdef CONFIG_PANEL_SMART_DIMMING
+	struct SmtDimInfo *smart_dimming_info = (struct SmtDimInfo *)panel->smart_dim_info;
+
+	if (smart_dimming_info == NULL) {
+		dsim_err("%s: dimming info is NULL\n", __func__);
+		goto get_elvess_err;
+	}
+#endif
 
 	if (dimming_info == NULL) {
 		dsim_err("%s : dimming info is NULL\n", __func__);
@@ -169,8 +190,18 @@ static unsigned char *get_elvss_from_index(struct dsim_device *dsim, int index, 
 	}
 
 	if(caps)
+#ifdef CONFIG_PANEL_SMART_DIMMING
+		if (panel->smart_on)
+			return (unsigned char *)smart_dimming_info[index].elvCaps;
+		else
+#endif
 		return (unsigned char *)dimming_info[index].elvCaps;
 	else
+#ifdef CONFIG_PANEL_SMART_DIMMING
+		if (panel->smart_on)
+			return (unsigned char *)smart_dimming_info[index].elv;
+		else
+#endif
 		return (unsigned char *)dimming_info[index].elv;
 
 get_elvess_err:
@@ -381,10 +412,20 @@ static int low_level_set_brightness(struct dsim_device *dsim ,int force)
 
 	dsim_panel_gamma_ctrl(dsim);
 
+#ifdef CONFIG_PANEL_SMART_DIMMING
+	if (panel->smart_on)
+		dsim_write_hl_data(dsim, S6E3HF4_SEQ_AOR_CONTROL, ARRAY_SIZE(S6E3HF4_SEQ_AOR_CONTROL));
+	else
+#endif
 	dsim_panel_aid_ctrl(dsim);
 
 	dsim_panel_set_elvss(dsim);
 
+#ifdef CONFIG_PANEL_SMART_DIMMING
+	if (panel->smart_on)
+		dsim_write_hl_data(dsim, S6E3HF4_SEQ_VINT_SET, ARRAY_SIZE(S6E3HF4_SEQ_VINT_SET));
+	else
+#endif
 	dsim_panel_set_vint(dsim, force);
 
 	dsim_panel_irc_ctrl(dsim);
