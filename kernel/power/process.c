dbb7a3eef034c0ac293b4982c6806e5ae93d5b9c
FAROVITUS <farovitus@gmail.com>
2017-05-26 19:35:24 -0400

    power: Reduce time to sleep
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -1,5 +1,5 @@
 /*
- * drivers/power/process.c - Functions for starting/stopping processes on 
+ * drivers/power/process.c - Functions for starting/stopping processes on
  *                           suspend transitions.
  *
  * Originally from swsusp.
@@ -20,10 +20,10 @@
 #include <trace/events/power.h>
 #include <linux/wakeup_reason.h>
 
-/* 
+/*
  * Timeout for stopping processes
  */
-unsigned int __read_mostly freeze_timeout_msecs = 20 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 2 * MSEC_PER_SEC;
 
 static int try_to_freeze_tasks(bool user_only)
 {
