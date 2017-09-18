3d82a82231630f0f9ffbcea07c9f9addd536d333
djb77 <dwayne.bakewell@gmail.com>
2017-09-18 17:01:19 +1000

    LINUX: 3.18.25 Kernel Update
    Conflicts:
          drivers/i2c/busses/i2c-s3c2410.c
          kernel/auditsc.c
          net/core/datagram.c
          security/keys/gc.c
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- net/core/datagram.c
+++ net/core/datagram.c
@@ -193,9 +193,7 @@ done:
 struct sk_buff *__skb_recv_datagram(struct sock *sk, unsigned int flags,
 				    int *peeked, int *off, int *err)
 {
-	struct sk_buff_head *queue = &sk->sk_receive_queue;
 	struct sk_buff *skb, *last;
-	unsigned long cpu_flags;
 	long timeo;
 	/*
 	 * Caller is allowed not to check sk->sk_err before skb_recv_datagram()
@@ -214,6 +212,8 @@ struct sk_buff *__skb_recv_datagram(struct sock *sk, unsigned int flags,
 		 * Look at current nfs client by the way...
 		 * However, this function was correct in any case. 8)
 		 */
+		unsigned long cpu_flags;
+		struct sk_buff_head *queue = &sk->sk_receive_queue;
 		int _off = *off;
 
 		last = (struct sk_buff *)queue;
@@ -256,8 +256,6 @@ struct sk_buff *__skb_recv_datagram(struct sock *sk, unsigned int flags,
 
 	return NULL;
 
-unlock_err:
-	spin_unlock_irqrestore(&queue->lock, cpu_flags);
 no_packet:
 	*err = error;
 	return NULL;
