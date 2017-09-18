e6a6c192463cd303a7240f9bdf74656e86d9d0d6
djb77 <dwayne.bakewell@gmail.com>
2017-09-18 17:07:41 +1000

    LINUX: 3.18.26 Kernel Update
    Conflicts:
          fs/ext4/super.c
          include/linux/jbd2.h
          net/core/sock.c
          net/ipv4/af_inet.c
          net/ipv6/af_inet6.c
          security/keys/keyctl.c
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -418,6 +418,9 @@ static void ext4_handle_error(struct super_block *sb, char* buf)
 		sb->s_flags |= MS_RDONLY;
 	}
 	if (test_opt(sb, ERRORS_PANIC)) {
+		if (EXT4_SB(sb)->s_journal &&
+		  !(EXT4_SB(sb)->s_journal->j_flags & JBD2_REC_ERR))
+			return;
 		panic("EXT4-fs (device %s): panic! %s\n",
 			sb->s_id, buf?buf:"no message");
 	}
@@ -647,6 +650,9 @@ void __ext4_abort(struct super_block *sb, const char *function,
 		save_error_info(sb, function, line);
 	}
 	if (test_opt(sb, ERRORS_PANIC) && !ignore_fs_panic) {
+		if (EXT4_SB(sb)->s_journal &&
+		  !(EXT4_SB(sb)->s_journal->j_flags & JBD2_REC_ERR))
+			return;
 		printk("EXT4-fs panic from previous error\n");
 		BUG_ON(1);
 	}
