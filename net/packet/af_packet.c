08f91ab795a9890b96fe60170058ef4856afef20
djb77 <dwayne.bakewell@gmail.com>
2018-01-26 01:43:14 +1100

    LINUX: 3.18.52 Kernel Update
    Conflicts:
          fs/f2fs/acl.c
          fs/f2fs/super.c
          mm/memory.c
          net/packet/af_packet.c
          sound/core/pcm_lib.c
    
    Did not patch drivers/usb/gadget/function/f_midi.c
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -3317,6 +3317,8 @@ packet_setsockopt(struct socket *sock, int level, int optname, char __user *optv
 			return -EINVAL;
 		if (copy_from_user(&val, optval, sizeof(val)))
 			return -EFAULT;
+		if (val > INT_MAX)
+			return -EINVAL;
 		lock_sock(sk);
 		if (po->rx_ring.pg_vec || po->tx_ring.pg_vec) {
 			ret = -EBUSY;
@@ -3829,6 +3831,8 @@ static int packet_set_ring(struct sock *sk, union tpacket_req_u *req_u,
 
 		rb->frames_per_block = req->tp_block_size/req->tp_frame_size;
 		if (unlikely(rb->frames_per_block <= 0))
+			goto out;
+		if (unlikely(req->tp_block_size > UINT_MAX / req->tp_block_nr))
 			goto out;
 		if (unlikely((rb->frames_per_block * req->tp_block_nr) !=
 					req->tp_frame_nr))
