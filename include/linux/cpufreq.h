cacd6a989083ee2c434da4803e9b857240fef108
djb77 <dwayne.bakewell@gmail.com>
2018-02-05 00:57:37 +1100

    cpufreq: intelliactive Governor
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -525,6 +525,9 @@ extern struct cpufreq_governor cpufreq_gov_electrodemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_HYPER;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_HYPER)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
